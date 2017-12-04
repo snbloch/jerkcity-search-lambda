19ea3629cd29ad07ec77c50045b8662c0114a902
Steven Bloch <snbloch@gmail.com>
2017-12-03 19:10:47 -0800

    Chat boss asked for this to return the image of the comic instead of the blockquote

--- index.py
+++ index.py
@@ -4,15 +4,22 @@ import urllib
 
 def jerksearch(event, context):
     url = 'https://www.jerkcity.com/search/?'
-    quotes = []
+    quotes = set([])
     if event.get('q') is None and len(event.get('text')) != 0:
         event['q'] = event.pop('text')
     querystring = urllib.urlencode(event)
     url = url + querystring
     page = pq(url=url)
-    quotes = [i.text() for i in page.items('.dialog')]
+    for i in page.items('a'):
+        if '.html' in i.attr['href']:
+            quotes.add(i.attr['href'])
     if len(quotes) > 0:
+        quotes = list(quotes)
         random_quote = quotes[randint(0, len(quotes) - 1)]
-        return random_quote
+        url = 'http://www.jerkcity.com' + random_quote
+        page = pq(url=url)
+        for i in page.items('.aidsy'):
+            image = i.attr['src']
+            return 'http://www.jerkcity.com' + image
     else:
         return 'No result found for query ' + event['q']
